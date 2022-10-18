ac4a7f95c2b5b66ebc388f3b8634887c1f6fa259
faturyuvi <faturyuvi19@gmail.com>
2022-10-18 09:15:16 +0700

    helloworld

---
+++ src/main/java/com/mycompany/helloworld/Helloworld.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author fatur
+ */
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Selamat Datang di Praktikum PBO");
+        System.out.println("Kelas 5 C");
+        
+    }
+}
